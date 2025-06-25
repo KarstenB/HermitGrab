16bcbf8e878e486d4667fbb9977548b9263e7d7f
Karsten Becker <skipper.pasty.0a@icloud.com>
2025-06-25 10:21:00 +0000

    Fix permissions

--- .devcontainer/cross-rust/install.sh
+++ .devcontainer/cross-rust/install.sh
@@ -5,13 +5,17 @@ rustup target add x86_64-pc-windows-gnu
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends musl-tools mingw-w64
 ARCH=$(uname -m)
-if [ "${aarch64}" == "aarch64" ]; then
+VENDOR="unknown"
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+if [ "${ARCH}" == "aarch64" ]; then
     sudo apt-get install -y --no-install-recommends gcc-x86-64-linux-gnu gcc-multilib-x86-64-linux-gnu
 fi
-if [ "${aarch64}" == "x86_64" ]; then
+if [ "${ARCH}" == "x86_64" ]; then
     sudo apt-get install -y --no-install-recommends gcc-aarch64-linux-gnu
 fi
 bash ./install-from-binstall-release.sh
-cargo binstall -y cargo-make
-cargo binstall -y cargo-nextest
-cargo binstall -y cargo-llvm-cov
+export BINSTALL_DISABLE_TELEMETRY=true
+export TARGETS="--targets $ARCH-$VENDOR-$OS-musl --targets $ARCH-$VENDOR-$OS-gnu"
+cargo binstall -y $TARGETS cargo-make
+cargo binstall -y $TARGETS cargo-nextest
+cargo binstall -y $TARGETS cargo-llvm-cov
