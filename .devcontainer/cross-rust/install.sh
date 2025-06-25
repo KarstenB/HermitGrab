7cbe7d2bf52095aa09ebfec08273730263e56b13
Karsten Becker <skipper.pasty.0a@icloud.com>
2025-06-25 18:03:02 +0000

    add libc-dev-*-cross

--- .devcontainer/cross-rust/install.sh
+++ .devcontainer/cross-rust/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 rustup target add aarch64-unknown-linux-musl
 rustup target add x86_64-unknown-linux-musl
 rustup target add x86_64-pc-windows-gnu
@@ -8,11 +9,21 @@ ARCH=$(uname -m)
 VENDOR="unknown"
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 if [ "${ARCH}" == "aarch64" ]; then
-    sudo apt-get install -y --no-install-recommends gcc-x86-64-linux-gnu gcc-multilib-x86-64-linux-gnu
+    sudo apt-get install -y --no-install-recommends gcc-x86-64-linux-gnu gcc-multilib-x86-64-linux-gnu libc6-dev-amd64-cross
 fi
 if [ "${ARCH}" == "x86_64" ]; then
-    sudo apt-get install -y --no-install-recommends gcc-aarch64-linux-gnu
+    sudo apt-get install -y --no-install-recommends gcc-aarch64-linux-gnu libc6-dev-arm64-cross
 fi
+
+# CURRDIR=$(pwd)
+# mkdir -p /tmp/testbuild
+# cargo new --bin testapp
+# cd testapp
+# cargo build --target aarch64-unknown-linux-musl
+# cargo build --target x86_64-unknown-linux-musl
+# cargo build --target x86_64-pc-windows-gnu
+# rm -rf /tmp/testbuild
+# cd $CURRDIR
 bash ./install-from-binstall-release.sh
 export BINSTALL_DISABLE_TELEMETRY=true
 export TARGETS="--targets $ARCH-$VENDOR-$OS-musl --targets $ARCH-$VENDOR-$OS-gnu"
